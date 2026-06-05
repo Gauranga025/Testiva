b0067de6815d66398bc22a83c9723ead21c37e87
Sarthak Debata <debatasarthak1@gmail.com>
2026-06-05 09:41:06 +0530

    Update middleware.ts

--- testiva/middleware.ts
+++ testiva/middleware.ts
@@ -1,20 +1,6 @@
 export const runtime = 'nodejs'
-
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-
-const isProtectedRoute = createRouteMatcher(['/workspace(.*)'])
-
-export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) await auth.protect()
-})
-
+import { clerkMiddleware } from '@clerk/nextjs/server'
+export default clerkMiddleware()
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-    // Always run for Clerk-specific frontend API routes
-    '/__clerk/(.*)',
-  ],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
 }
